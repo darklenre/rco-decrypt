e68474dec5f92e5f560ad6407f9b9a4b177b0516
JakeeLuwi <jeloubirad@gmail.com>
2025-06-12 21:55:10 +0800

    update script

--- rcoDecrypt.js
+++ rcoDecrypt.js
@@ -11,7 +11,7 @@ const pageLinks = new Array();
 
 //funniRegex(/var\s+(_[^\s=]+mvn)\s*(?:=\s*[^;]+)?\s*;/);
 //funniRegex(/var\s+(_[^\s=]+mxn)\s*(?:=\s*[^;]+)?\s*;/);
-funniRegex(/var\s+(_areyoufree)\s*(?:=\s*[^;]+)?\s*;/);
+funniRegex(/var\s+(_(?!.*mxn)[a-zA-Z0-9]+)\s*=\s*'rcoz'\s*;/);
 //funniRegex(/var\s+(c_[^\s=]+)\s*(?:=\s*[^;]+)?\s*;/g, true);
 //funniRegex(/var\s+(_[^\s=]+)\s*=\s*new\s+Array\(\)\s*;/g, true);
 
